c6ee282c51645aabe7865b395366fe76ba7fdb6d
Marrk <56095015+TheRealMarrk@users.noreply.github.com>
2022-11-05 15:14:04 -0300

    DarkMode and Volumes

--- script.js
+++ script.js
@@ -10,17 +10,27 @@ const treeUnclicked = document.querySelector('.unclicked')
 let timeTimeout
 const minutesOg = Number(minutesDisplay.textContent)
 
+// Dark mode
+const dark = document.querySelector(".dark")
+const light = document.querySelector(".light")
+const body = document.querySelector("body")
+
 // Sounds controls 
 const tree = document.querySelector(".tree")
 const rain = document.querySelector(".rain")
 const coffeShop = document.querySelector(".coffeShop")
 const fireplace = document.querySelector(".fireplace")
+const vol = document.querySelector('input')
 
 const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
 const floresta = new Audio("./sounds/Floresta.wav")
 const chuva = new Audio("./sounds/Chuva.wav")
 const cafeteria = new Audio("./sounds/Cafeteria.wav")
 const lareira = new Audio("./sounds/Lareira.wav")
+const volumeFlorest = document.querySelector('#volume-florest')
+const volumeRain = document.querySelector('#volume-rain')
+const volumeCoffeShop = document.querySelector('#volume-coffeShop')
+const volumeFireplace = document.querySelector('#volume-fireplace')
 
 function florestSound() {
   floresta.loop = true
@@ -63,6 +73,9 @@ soundStop = () => {
   floresta.pause()
   chuva.pause()
   cafeteria.pause()
+}
+
+function volumeDefault() { volumeFlorest.value = 50
 }
 
 
@@ -154,4 +167,33 @@ coffeShop.addEventListener("click", () => {
 })
 fireplace.addEventListener("click", () => {
   fireplaceSound()
+})
+
+// dark mode buttons
+
+dark.addEventListener("click", () => {
+  dark.classList.add('hide')
+  light.classList.remove('hide')
+  body.classList.add("darkMode")
+})
+
+light.addEventListener("click", () => {
+  light.classList.add('hide')
+  dark.classList.remove('hide')
+  body.classList.remove('darkMode')
+})
+
+// volumes
+
+volumeFlorest.addEventListener("change", () => {
+  floresta.volume = Number(volumeFlorest.value)/100
+})
+volumeRain.addEventListener("change", () => {
+  chuva.volume = Number(volumeRain.value)/100
+})
+volumeCoffeShop.addEventListener("change", () => {
+  cafeteria.volume = Number(volumeCoffeShop.value)/100
+})
+volumeFireplace.addEventListener("change", () => {
+  lareira.volume = Number(volumeFireplace.value)/100
 })
